9aee5d8c93cb184f7835293cc877e0979a14108b
Lungisani Majozi <lungisaniimajozi@gmail.com>
2025-08-21 01:43:43 +0200

    Make contact form fields required and update styles

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -20,23 +20,27 @@ const Contacts = () => {
             lungisaniimajozi@gmail.com
           </p>
         </div>
+
         <input
           className="bg-[#ccd6f6] p-2 rounded-lg"
           type="text"
           placeholder="Name"
           name="name"
+          required
         />
         <input
           className="my-4 p-2 bg-[#ccd6f6] rounded-lg"
           type="email"
           placeholder="Email@gmail.com"
           name="email"
+          required
         />
         <textarea
           className="bg-[#ccd6f6] p-2 rounded-lg"
           name="message"
           rows="10"
           placeholder="Message"
+          required
         ></textarea>
         <button className="text-white border-2 hover:bg-purple-600 hover:border-purple-600 px-4 py-3 my-8 mx-auto flex items-center">
           Let's Collaborate
