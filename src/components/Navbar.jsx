f542798b291a265d5c5d8d2ac7ce1afc0c9a809f
Lungisani Majozi <lungisaniimajozi@gmail.com>
2025-08-21 02:38:47 +0200

    Make contact form fields required and update styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,33 +58,33 @@ const Navbar = () => {
         className={
           !nav
             ? "hidden"
-            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center border-4 border-w- border-purple-600 rounded-lg"
         }
       >
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-4xl border-b-2 border-purple-600 ">
           <Link onClick={handleClick} to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-4xl border-b-2 border-purple-600">
           {" "}
           <Link onClick={handleClick} to="about" smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-4xl border-b-2 border-purple-600">
           {" "}
           <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-4xl border-b-2 border-purple-600">
           {" "}
           <Link onClick={handleClick} to="work" smooth={true} duration={500}>
             Work
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-4xl border-b-2 border-purple-600">
           {" "}
           <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
             Contact
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,7 +4,10 @@ import MYP from "../assets/MYP.png";
 import Myf from "../assets/MYF.png";
 const Work = () => {
   return (
-    <div name="work" className="w-full bg-[#0a192f] md:h-screen text-gray-300">
+    <div
+      name="work"
+      className="w-full bg-[#0a192f] md:h-screen text-gray-300 pt-36"
+    >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-purple-500 text-gray-300">
